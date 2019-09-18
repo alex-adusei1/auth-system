bc45d32391d3c04941813828996a147e6a21c583
ucwealth <ucheokeke16@gmail.com>
2019-09-19 00:16:32 +0100

    remember-me added

--- App/Controller/Auth.php
+++ App/Controller/Auth.php
@@ -50,3 +50,26 @@ class Auth
         return false;
     }
 }
+
+    //Remember me  
+
+    if(isset($_POST['email']) && isset($_POST["password"]))
+    {
+        if(!empty($_POST["remember"]))
+        {
+            setcookie("email", $_POST["email"], time() + (30 * 24 * 60 * 60));
+            setcookie("password", $_POST["password"], time() + (30 * 24 * 60 * 60));
+        }
+        else
+        {
+            if(isset($_COOKIE["email"]))
+            {
+                setcookie("email", "");
+            }
+            if(isset($_COOKIE["password"]))
+            {
+                setcookie("password", "");
+            }
+        }
+        header("location:index.php");
+    }
