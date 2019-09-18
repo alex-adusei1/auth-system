71ec931051dca9b36c819a48bbaacf5e73abfcd1
alex_adusei1 <alex.a.adusei@gmail.com>
2019-09-18 08:57:42 +0000

    updates on signup ui

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <title>Sign Up</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <script src="https://kit.fontawesome.com/cb6426b160.js"></script>
@@ -16,9 +16,18 @@
                 <img class="mb-4" src="images/brand.png" alt="brand" width="100" height="100">
             </span>
                 <div class="header">
-                <h1 class="h2 mb-3 font-weight-normal">Please Login</h1>
+                <h1 class="h2 mb-3 font-weight-normal">Create an account</h1>
             </div>
-        
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="fullname"> 
+                        <i class="fa fa-user"></i>
+                    </span>
+                </div>
+                <input type="text" class="form-control" placeholder="fullname" aria-label="fullname" aria-describedby="fullname">
+            </div>
+
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="email"> 
@@ -37,22 +46,14 @@
                 <input type="password" class="form-control" placeholder="Password" aria-label="password" aria-describedby="password">
             </div>
 
-            <div class="checkbox mb-3">
-                <label>
-                    <input type="checkbox" value="remember-me"> 
-                    <span class="font-weight-normal wh-tx"> Remember me </span>
-                </label>
-            </div>
             <div class="form-group"> 
                 <button type="submit" class="btn btn-success btn-lg btn-block">
-                    Login
+                Sign Up
                 </button>
             </div>
-            
         </form>
-        <span class="extras">
-            <a href="#" class="li-lf">Forgot password?</a>
-            <a href="signup.php" class="li-rg">Don't have an account?</a> 
+        <span>
+            <a href="index.php" class="lo-g">return to sign in page</a>
         </span>
 </body>
 </html>
